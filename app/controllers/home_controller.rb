9ba1c4b7898fac5c5112bdb4d2b51197246b4f02
landovsky <landovsky@gmail.com>
2017-10-27 22:16:17 +0200

    - basic home group card design - dummy data

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,18 @@
 class HomeController < ApplicationController
-  def index; end
+  def index
+    @groups = [
+      OpenStruct.new(day: 'Monday',
+                     name: 'Family group',
+                     description: 'Kids friendly group for parents and kids. We start and finish early.',
+                     count: 5),
+      OpenStruct.new(day: 'Tuesday',
+                     name: 'Youth group',
+                     description: 'We are cool.',
+                     count: 8),
+      OpenStruct.new(day: 'Thursday',
+                     name: 'Sports group',
+                     description: 'Climbing, running, soccer, beach volleyball, what have you..',
+                     count: 4)
+    ]
+  end
 end
